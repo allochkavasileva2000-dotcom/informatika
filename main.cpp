0725aa9cac1a67d6cee7ee74d8108ed17e66e238
allochkavasileva2000-dotcom <allochka_vasileva_2000@bk.ru>
2025-09-30 00:08:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <locale>
+#include <cmath>
+
+
+int main() {
+    setlocale(LC_ALL,"Russian");
+    float x,y;
+    std::cout<< "введите x , y" << std::endl;
+    std::cin >> x >> y;
+    float R = (-x-sqrt(pow(x,2) - 4*x*y))/(2*y);
+    float S = log(pow(2,x)) - tan(fmin(x,y));
+    std::cout<< "R = "<< R << std::endl;
+    std::cout<< "S = "<< S << std::endl;
+    float C = fmax(R,S);
+    std::cout<<"C = "<< C <<std::endl;
+    
+    
+return 0;
+}
+    
